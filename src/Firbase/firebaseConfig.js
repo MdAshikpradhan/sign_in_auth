4e96a30bfdb3d63e95985160f216b97d016fc09f
mdAshikPradhan <mdashikpradhan.com>
2021-03-16 19:41:26 +0600

    firebase app

---
+++ src/Firbase/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCoNBuzB1iwCHlIzXF5IJh2P-acL9U0N3A",
+    authDomain: "ema-john-simple-70578.firebaseapp.com",
+    projectId: "ema-john-simple-70578",
+    storageBucket: "ema-john-simple-70578.appspot.com",
+    messagingSenderId: "492400716073",
+    appId: "1:492400716073:web:331fd674f4bf23e2b4ef38",
+    measurementId: "G-CMK3H8398Q"
+  };
